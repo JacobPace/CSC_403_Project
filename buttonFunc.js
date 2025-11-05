7c4bb819532e8e5fd852849c5a5647c760b67cb7
yuelia02 <128103232+yuelia02@users.noreply.github.com>
2025-11-05 02:45:15 -0600

    Clean up code structure and inconsistencies
    Made changes based off of review notes in task-1c (Google Spreadsheet). Also fixed inconsistencies in variable and function names.

--- buttonFunc.js
+++ buttonFunc.js
@@ -12,68 +12,74 @@ class State {
     }
 }
 
-function main(){
-    let Alabama = new State("Alabama", false, []);
-    let Alaska = new State("Alaska", false, []);
-    let Arizona = new State("Arizona", false, []);
-    let Arkansas = new State("Akansas", false, []);
-    let California = new State("California", false, []);
-    let Colorado = new State("Colorado", false, []);
-    let Conneticut = new State("Conneticut", false, []);
-    let Delaware = new State("Delaware", false, []);
-    let Florida = new State("Florida", false, []);
-    let Georgia = new State("Georgia", false, []);
-    let Hawaii = new State("Hawaii", false, []);
-    let Idaho = new State("Idaho", false, []);
-    let Illinois = new State("Illinois", false, []);
-    let Indiana = new State("Indiana", false, []);
-    let Iowa = new State("Iowa", false, []);
-    let Kansas = new State("Kansas", false, []);
-    let Kentucky = new State("Kentucky", false, []);
-    let Louisiana = new State("Louisiana", false, []);
-    let Maine = new State("Maine", false, []);
-    let Maryland = new State("Maryland", false, []);
-    let Massachusetts = new State("Massachusetts", false, []);
-    let Michigan = new State("Michigan", false, []);
-    let Minnesota = new State("Minnesota", false, []);
-    let Mississippi = new State("Mississippi", false, []);
-    let Missouri = new State("Missouri", false, []);
-    let Montana = new State("Montana", false, []);
-    let Nebraska = new State("Nebraska", false, []);
-    let Nevada = new State("Nevada", false, []);
-    let New_Hampshire = new State("New Hampshire", false, []);
-    let New_Jersey = new State("New Jersey", false, []);
-    let New_Mexico = new State("New Mexico", false, []);
-    let New_York = new State("New York", false, []);
-    let North_Carolina = new State("North Carolina", false, []);
-    let North_Dakota = new State("North Dakota", false, []);
-    let Ohio = new State("Ohio", false, []);
-    let Oklahoma = new State("Oklahoma", false, []);
-    let Oregon = new State("Oregon", false, []);
-    let Pennsylvania = new State("Pennsylvania", false, []);
-    let Rhode_Island = new State("Rhode Island", false, []);
-    let South_Carolina = new State("South Carolina", false, []);
-    let South_Dakota = new State("South Dakota", false, []);
-    let Tennessee = new State("Tennessee", false, []);
-    let Texas = new State("Texas", false, []);
-    let Utah = new State("Utah", false, []);
-    let Vermont = new State("Vermont", false, []);
-    let Virginia = new State("Virginia", false, []);
-    let Washington = new State("Washington", false, []);
-    let West_Virginia = new State("West Virginia", false, []);
-    let Wisconsin = new State("Wisconsin", false, []);
-    let Wyoming = new State("Wyoming", false, []);
-    setAllNeighbors();
-}
+let Alabama = new State("Alabama", false, []);
+let Alaska = new State("Alaska", false, []);
+let Arizona = new State("Arizona", false, []);
+let Arkansas = new State("Akansas", false, []);
+let California = new State("California", false, []);
+let Colorado = new State("Colorado", false, []);
+let Conneticut = new State("Conneticut", false, []);
+let Delaware = new State("Delaware", false, []);
+let Florida = new State("Florida", false, []);
+let Georgia = new State("Georgia", false, []);
+let Hawaii = new State("Hawaii", false, []);
+let Idaho = new State("Idaho", false, []);
+let Illinois = new State("Illinois", false, []);
+let Indiana = new State("Indiana", false, []);
+let Iowa = new State("Iowa", false, []);
+let Kansas = new State("Kansas", false, []);
+let Kentucky = new State("Kentucky", false, []);
+let Louisiana = new State("Louisiana", false, []);
+let Maine = new State("Maine", false, []);
+let Maryland = new State("Maryland", false, []);
+let Massachusetts = new State("Massachusetts", false, []);
+let Michigan = new State("Michigan", false, []);
+let Minnesota = new State("Minnesota", false, []);
+let Mississippi = new State("Mississippi", false, []);
+let Missouri = new State("Missouri", false, []);
+let Montana = new State("Montana", false, []);
+let Nebraska = new State("Nebraska", false, []);
+let Nevada = new State("Nevada", false, []);
+let New_Hampshire = new State("New Hampshire", false, []);
+let New_Jersey = new State("New Jersey", false, []);
+let New_Mexico = new State("New Mexico", false, []);
+let New_York = new State("New York", false, []);
+let North_Carolina = new State("North Carolina", false, []);
+let North_Dakota = new State("North Dakota", false, []);
+let Ohio = new State("Ohio", false, []);
+let Oklahoma = new State("Oklahoma", false, []);
+let Oregon = new State("Oregon", false, []);
+let Pennsylvania = new State("Pennsylvania", false, []);
+let Rhode_Island = new State("Rhode Island", false, []);
+let South_Carolina = new State("South Carolina", false, []);
+let South_Dakota = new State("South Dakota", false, []);
+let Tennessee = new State("Tennessee", false, []);
+let Texas = new State("Texas", false, []);
+let Utah = new State("Utah", false, []);
+let Vermont = new State("Vermont", false, []);
+let Virginia = new State("Virginia", false, []);
+let Washington = new State("Washington", false, []);
+let West_Virginia = new State("West Virginia", false, []);
+let Wisconsin = new State("Wisconsin", false, []);
+let Wyoming = new State("Wyoming", false, []);
+
+const AllStates = [Alabama, Alaska, Arizona, Arkansas, California, Colorado, Conneticut,
+    Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky,
+    Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri,
+    Montana, Nebraska, Nevada, New_Hampshire, New_Jersey, New_Mexico, New_York, North_Carolina,
+    North_Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode_Island, South_Carolina, South_Dakota,
+    Tennessee, Texas, Utah, Vermont, Virginia, Washington, West_Virginia, Wisconsin, Wyoming];
+
+setAllNeighbors();
 
 function setAllNeighbors(){
     Alabama.setNeighbors([Mississippi, Florida, Georgia, Tennessee]);
-    Alaska.setNeighbors([Washinton, Idaho, Montana]);
+    Alaska.setNeighbors([Washington, Idaho, Montana]);
     Arizona.setNeighbors([California, Nevada, Utah, Colorado, New_Mexico]);
-    Arkansas,setNeighbors([Mississippi, Missouri, Oklahoma, Louisiana, Tennessee, Texas]);
+    Arkansas.setNeighbors([Mississippi, Missouri, Oklahoma, Louisiana, Tennessee, Texas]);
     California.setNeighbors([Oregon, Nevada, Arizona]);
     Colorado.setNeighbors([Wyoming, Nebraska, Kansas, Oklahoma, Utah, New_Mexico, Arizona]);
-    Conneticut.setNeighbors([New_York, Massachusetts, Rhode_Island]);
+    Connecticut.setNeighbors([New_York, Massachusetts, Rhode_Island]);
     Delaware.setNeighbors([Maryland, New_Jersey, Pennsylvania]);
     Florida.setNeighbors([Georgia, Alabama]);
     Georgia.setNeighbors([Tennessee, North_Carolina, South_Carolina, Florida, Alabama]);
@@ -87,9 +93,9 @@ function setAllNeighbors(){
     Louisiana.setNeighbors([Arkansas, Mississippi, Texas]);
     Maine.setNeighbors([New_Hampshire, Massachusetts]);
     Maryland.setNeighbors([West_Virginia, Pennsylvania, Delaware, Virginia]);
-    Massachusetts.setNeighbors([Maine, New_Hampshire, Vermont, New_York, Conneticut, Rhode_Island]);
+    Massachusetts.setNeighbors([Maine, New_Hampshire, Vermont, New_York, Connecticut, Rhode_Island]);
     Michigan.setNeighbors([Wisconsin, Indiana, Ohio]);
-    Minnesota.setNeighbors([North_Dakota, South_Dakota, Wisonsin, Iowa]);
+    Minnesota.setNeighbors([North_Dakota, South_Dakota, Wisconsin, Iowa]);
     Mississippi.setNeighbors([Arkansas, Louisiana, Tennessee, Alabama]);
     Missouri.setNeighbors([Iowa, Illinois, Kentucky, Tennessee, Arkansas, Oklahoma, Kansas, Nebraska]);
     Montana.setNeighbors([Alaska, North_Dakota, South_Dakota, Wyoming, Idaho]);
@@ -98,14 +104,14 @@ function setAllNeighbors(){
     New_Hampshire.setNeighbors([Maine, Vermont, Massachusetts]);
     New_Jersey.setNeighbors([New_York, Pennsylvania, Delaware]);
     New_Mexico.setNeighbors([Colorado, Oklahoma, Texas, Arizona, Utah]);
-    New_York.setNeighbors([Vermont, Massachusetts, Conneticut, Rhode_Island, New_Jersey, Pennsylvania]);
+    New_York.setNeighbors([Vermont, Massachusetts, Connecticut, Rhode_Island, New_Jersey, Pennsylvania]);
     North_Carolina.setNeighbors([Virginia, Tennessee, South_Carolina]);
     North_Dakota.setNeighbors([Minnesota, South_Dakota, Montana]);
     Ohio.setNeighbors([Michigan, Pennsylvania, West_Virginia, Kentucky, Indiana]);
     Oklahoma.setNeighbors([Kansas, Arkansas, Texas, New_Mexico, Colorado]);
-    Oregon.setNeighbors([Washinton, Idaho, Nevada, California]);
+    Oregon.setNeighbors([Washington, Idaho, Nevada, California]);
     Pennsylvania.setNeighbors([New_York, New_Jersey, Maryland, West_Virginia, Ohio]);
-    Rhode_Island.setNeighbors([Massachusetts, Conneticut, New_York]);
+    Rhode_Island.setNeighbors([Massachusetts, Connecticut, New_York]);
     South_Carolina.setNeighbors([North_Carolina, Georgia]);
     South_Dakota.setNeighbors([North_Dakota, Minnesota, Iowa, Nebraska, Wyoming, Montana]);
     Tennessee.setNeighbors([Kentucky, Virginia, North_Carolina, Georgia, Alabama, Mississippi, Arkansas, Missouri]);
@@ -132,17 +138,10 @@ function nextState(){
    }
 }
 
-const AllStates = [Alabama, Alaska, Arizona, Arkansas, California, Colorado, Conneticut,
-    Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky,
-    Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri,
-    Montana, Nebraska, Nevada, New_Hampshire, New_Jersey, New_Mexico, New_York, North_Carolina,
-    North_Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode_Island, South_Carolina, South_Dakota,
-    Tennessee, Texas, Utah, Vermont, Virginia, Washington, West_Virginia, Wisconsin, Wyoming];
-
 // Returns true/false if all states have been visited
 function allVisited(){
     for (let i = 0; i < 49; i++){
-        if (AllSates[i].visited == false){
+        if (AllStates[i].visited == false){
             return false;
         }
     return true;
@@ -152,7 +151,7 @@ function allVisited(){
 // Returns true/false if any state has been visited
 function anyVisited(){
     for (let i = 0; i < 49; i++){
-        if(AllSates[i].visited == true){
+        if(AllStates[i].visited == true){
             return true;
         }
     }
@@ -176,12 +175,12 @@ function findNeighbor(state){
     for(let j = 0; j < state.neighbors.length - 1; j++){
         return findNeighbor(state.neighbors[j]);
     }
-    
 }
+
 let currentState;
 function selectState(){
     // If all states have been visited, do the celebration thingy
-    if(AllVisited()){
+    if(allVisited()){
         return;
     }
     // If any state has already been visited, move to its neighbor
@@ -190,8 +189,9 @@ function selectState(){
     }
     // No state has been visited, randomly select a starting point
     else{
-        currentState = AllStates[getRandomInt(0, 49));
+        currentState = AllStates[getRandomInt(0, 49)];
     }
 }
+
 
 
